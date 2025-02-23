7c2586084eab26f5589837fc01e4eebab5f95bfa
Hyeonjae Lee <zanggu3@nate.com>
2025-02-23 22:49:51 +0900

    feat: 날짜 표기 방식 수정

--- composeApp/src/commonMain/kotlin/xyz/hyeonjae/quollect/feature/main/viewdata/mapper/SpeakerViewDataMapper.kt
+++ composeApp/src/commonMain/kotlin/xyz/hyeonjae/quollect/feature/main/viewdata/mapper/SpeakerViewDataMapper.kt
@@ -2,6 +2,7 @@ package xyz.hyeonjae.quollect.feature.main.viewdata.mapper
 
 import xyz.hyeonjae.quollect.dto.SpeakerDto
 import xyz.hyeonjae.quollect.feature.main.viewdata.SpeakerViewData
+import kotlin.math.abs
 
 class SpeakerViewDataMapper {
     fun viewData(dto: SpeakerDto?): SpeakerViewData? {
@@ -16,10 +17,18 @@ class SpeakerViewDataMapper {
 
     private fun calculateLifespan(birthYear: Int?, deathYear: Int?): String {
         return when {
-            birthYear != null && deathYear != null -> "$birthYear - $deathYear"
-            birthYear != null -> "$birthYear"
-            deathYear != null -> "$deathYear"
+            birthYear != null && deathYear != null -> "${yearText(birthYear)} - ${yearText(deathYear)}"
+            birthYear != null -> "${yearText(birthYear)} - "
+            deathYear != null -> yearText(deathYear)
             else -> ""
+        }
+    }
+
+    private fun yearText(year: Int): String {
+        return if (year < 0) {
+            "기원전 ${abs(year)}"
+        } else {
+            "$year"
         }
     }
 }
